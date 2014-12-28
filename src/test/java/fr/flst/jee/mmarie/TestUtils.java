08ef27c216bbd93bcfc72313f48cd5efc28a70ae
maximilien marie <maximilien.marie@isen-lille.fr>
2014-12-28 12:55:16 +0100

    test(hibernate): setup hibernate test protocol

---
+++ src/test/java/fr/flst/jee/mmarie/TestUtils.java
@@ -0,0 +1,68 @@
+package fr.flst.jee.mmarie;
+
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.XmlDataSet;
+import org.dbunit.operation.DatabaseOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Created by Maximilien on 01/10/2014.
+ */
+public class TestUtils {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestUtils.class);
+    public static final String FIXTURES_FILE = "data-dbunit.xml";
+
+    private static Connection connection;
+    private static IDataSet dbUnitDataset;
+
+    public static Connection getJDBCConnection(){
+        try{
+            if (connection == null){
+                Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance();
+                connection = DriverManager.getConnection("jdbc:derby:memory:BookStoreDB;");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void reloadDbUnitData() throws Exception {
+        Connection connection = getJDBCConnection();
+
+        PreparedStatement stmt = connection
+                .prepareStatement("ALTER TABLE AUTHORS ALTER COLUMN id RESTART WITH 1");
+        stmt.execute();
+        stmt.close();
+
+		/* load the first time only */
+        if (dbUnitDataset == null){
+            dbUnitDataset = new XmlDataSet(Thread.currentThread()
+                    .getContextClassLoader().getResourceAsStream(FIXTURES_FILE));
+        }
+
+        // Clean the data from previous test and insert new data test.
+        DatabaseOperation.CLEAN_INSERT.execute(new DatabaseConnection(connection), dbUnitDataset);
+
+        connection.commit();
+        closeJDBCConnection();
+    }
+
+    public static void closeJDBCConnection(){
+        if (connection != null){
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                LOGGER.error("Impossible to close sql connection");
+            }
+            connection = null;
+        }
+    }
+}
---
+++ src/test/java/fr/flst/jee/mmarie/db/dao/hibernate/AbstractTestCase.java
@@ -0,0 +1,61 @@
+package fr.flst.jee.mmarie.db.dao.hibernate;
+
+import fr.flst.jee.mmarie.TestUtils;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ * Created by Maximilien on 01/10/2014.
+ */
+public class AbstractTestCase {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractTestCase.class);
+
+    protected static EntityManagerFactory emf;
+    protected static EntityManager em;
+    protected static SessionFactory sessionFactory;
+
+    private Transaction currentTransaction;
+    @BeforeClass
+    public static void initClass() throws Exception {
+        emf = Persistence.createEntityManagerFactory("BookstorePU");
+
+        sessionFactory = emf.unwrap(SessionFactory.class);
+    }
+
+    @AfterClass
+    public static void closeClass() {
+        emf.close();
+        TestUtils.closeJDBCConnection();
+    }
+
+    @Before
+    public void initTest() throws Exception {
+        //ask dbUnit to store them in db
+        TestUtils.reloadDbUnitData();
+        em = emf.createEntityManager();
+        LOGGER.debug("BEFORE TEST");
+        em.getTransaction().begin();
+        currentTransaction = sessionFactory.getCurrentSession().beginTransaction();
+    }
+
+    @After
+    public void closeTest() {
+        LOGGER.debug("AFTER TEST");
+        em.getTransaction().commit();
+        currentTransaction.commit();
+        em.clear();
+        em.close();
+    }
+}
