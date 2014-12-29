679cbe4c643b2f96b1b5cdac914db0e607e5801a
maximilien marie <maximilien.marie@isen-lille.fr>
2014-12-29 21:29:11 +0100

    test(guice): misc module

---
+++ src/test/java/fr/flst/jee/mmarie/misc/MiscModuleTest.java
@@ -0,0 +1,22 @@
+package fr.flst.jee.mmarie.misc;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class MiscModuleTest {
+
+    private MiscModule miscModule = new MiscModule();
+
+    @Test
+    public void testConfigure() throws Exception {
+
+    }
+
+    @Test
+    public void testProvidesMapper() throws Exception {
+        assertThat(miscModule.providesMapper(), is(notNullValue()));
+    }
+}
