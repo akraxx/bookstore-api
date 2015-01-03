f0b0765958f7a798e621ad88f758965f7f3c4a1c
maximilien marie <maximilien.marie@isen-lille.fr>
2015-01-03 19:44:20 +0100

    test(dao): orders by login

--- src/test/java/fr/flst/jee/mmarie/db/dao/hibernate/HibernateOrderDAOTest.java
+++ src/test/java/fr/flst/jee/mmarie/db/dao/hibernate/HibernateOrderDAOTest.java
@@ -2,13 +2,17 @@ package fr.flst.jee.mmarie.db.dao.hibernate;
 
 import com.google.common.base.Optional;
 import fr.flst.jee.mmarie.core.Order;
+import fr.flst.jee.mmarie.core.OrderLine;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.comparesEqualTo;
+import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -30,4 +34,26 @@ public class HibernateOrderDAOTest extends AbstractDAOTestCase {
                 hasProperty("orderDate", comparesEqualTo(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2014-12-26 14:15:00"))),
                 hasProperty("mailingAddress", hasProperty("id", is(2)))));
     }
+
+    @Test
+    public void testFindByUserLogin() throws Exception {
+        List<Order> byUserLogin = hibernateOrderDAO.findByUserLogin("yhovart");
+        assertThat(byUserLogin, hasItems(
+                        allOf(
+                                Matchers.<Order>hasProperty("user", hasProperty("login", is("yhovart"))),
+                                Matchers.<Order>hasProperty("orderDate",
+                                        comparesEqualTo(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2014-12-26 14:15:00"))),
+                                Matchers.<Order>hasProperty("mailingAddress", hasProperty("id", is(2)))
+                        ),
+                        allOf(
+                                Matchers.<Order>hasProperty("user", hasProperty("login", is("yhovart"))),
+                                Matchers.<Order>hasProperty("orderDate",
+                                        comparesEqualTo(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2014-12-27 17:31:10"))),
+                                Matchers.<Order>hasProperty("mailingAddress", hasProperty("id", is(1)))
+                        )
+                )
+        );
+
+    }
+
 }
